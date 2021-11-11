547b2c3cd4f320743718407c008df20b52fac062
Giovanni <nottheworstdev@gmail.com>
2021-11-11 23:47:36 +0100

    Adds mobile app

---
+++ RateMyAir/RateMyAir.Mobile/RateMyAir.Mobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using RateMyAir.Mobile.ViewModels;
+using RateMyAir.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace RateMyAir.Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
